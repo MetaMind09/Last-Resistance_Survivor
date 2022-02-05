17fdb4067df5afd682e81eed8326ea3ab8c7b842
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2022-02-06 02:22:59 +0500

    Add files via upload

---
+++ source/gamestrings/Survivor_ruRU.lua
@@ -0,0 +1,6 @@
+kSurvivorLocaleMessages = {}
+kSurvivorLocaleMessages["MARINE_TEAM_GAME_STARTED"] = "УБИВАЙТЕ ВАШИХ СОСЕДЕЙ"
+kSurvivorLocaleMessages["MARINE_TEAM_SURVIVAL_STARTED"] = "ВЫЖИВИТЕ"
+kSurvivorLocaleMessages["ALIEN_TEAM_SURVIVAL_STARTED"] = "АССИМИЛИРУЙТЕ ИХ"
+kSurvivorLocaleMessages["GAME_START_COMMANDERS"] = "Игра начнётся, когда все игроки присоединятся к космодесанту."
+kSurvivorLocaleMessages["PLAYER_MUTATED"] = "%s МУТИРОВАНО"
